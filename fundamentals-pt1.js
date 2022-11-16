2c84b1a072c78b0be1c30a85ddde9d687803fade
ElamineBendaas <33202205+Elaminebendaas@users.noreply.github.com>
2022-11-16 15:51:02 -0500

    Finished Part 1 Practice

---
+++ fundamentals-pt1.js
@@ -0,0 +1,13 @@
+console.log(23 + 97)
+console.log(6+8+6+21+98+1) //Output: 131
+console.log((4+6+9)/77) //Output: 0.24675
+
+let a = 10
+console.log(a)
+console.log(9*a)
+let b = 7 * a
+console.log(b)
+
+const max = 57
+let actual = max -13
+let percentage = actual/max
